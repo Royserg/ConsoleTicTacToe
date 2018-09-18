4a93f83ccb480fddcd2a37b02c9faaa4f9c4f8d1
Royserg <staniojakub@gmail.com>
2018-09-18 22:22:19 +0200

    Implement main game loop + methods

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class Game {
 
+    private static int roundNum = 1;
+    private static Player playing;
+    private static boolean isRunning;
+    // initialize Board instance
+    private static Board board = new Board();
+
     public static void main(String[] args) {
         System.out.println(">>> Tic Tac Toe <<<");
 
@@ -9,12 +15,40 @@ public class Game {
         String name1 = userInput("Player 1 name: ");
         String name2 = userInput("Player 2 name: ");
 
-        /* create Player objects */
-        Player player1 = new Player(name1);
-        Player player2 = new Player(name2);
+        /* create Player instances */
+        Player player1 = new Player(name1, 'O');
+        Player player2 = new Player(name2, 'X');
 
-        System.out.println(player1.getName());
-        System.out.println(player2.getName());
+        /* === Starting Game === */
+        isRunning = true;
+        playing = player1;
+
+        while (isRunning) {
+            // info
+            showRound();
+            // show board
+            board.drawBoard();
+            // user moves
+            System.out.print(playing.getName());
+            int spot = Integer.parseInt(userInput(", make your move: "));
+
+            playerMove(spot);
+
+            // == end - check winner ==
+            String winCheck = board.checkWinner();
+
+            if (winCheck == "Tie") {
+                isRunning = false;
+                System.out.println("It is a Tie!!!");
+                System.out.println("YOu wanna play again?");
+                // some input awaiting magic TODO
+            }
+
+            // change player
+            playing = (playing == player1) ? player2 : player1;
+        }
+
+        System.out.println("Shouldn't arrive here");
 
     }
 
@@ -25,4 +59,18 @@ public class Game {
 
         return kbd.nextLine();
     }
+
+    private static void showRound() {
+        System.out.println("============");
+        System.out.println("= Round: " + roundNum + " =");
+        System.out.println("============");
+    }
+
+    private static void playerMove(int spot) {
+        // validate provided spot TODO
+
+        // mark the board
+        board.markSpot(spot, playing.getMarker());
+    }
+
 }
